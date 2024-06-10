166d325a2eedec618f6ae87c4c322e41da8e16ea
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-10 14:20:52 +0530

    Request and Response Handler

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const { handleGenerateNewShortURL, handleGetAnalytics } = require("../controllers/url")
+
 const router = express.Router();
 
-router.post("/");
+router.post("/", handleGenerateNewShortURL);
 
+router.get("/analytics/:shortId", handleGetAnalytics)
 
+module.exports = router;
