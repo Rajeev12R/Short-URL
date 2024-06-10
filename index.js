40cddf55133e30eac009f0dfd923b528494f361e
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-10 21:41:34 +0530

    Setup Express app with routes for URL shortening and MongoDB connection

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const path = require("path");
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8001;
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+
+    })
+});
 
 const urlRoute = require("./routes/url");
 
@@ -16,9 +26,16 @@ const URL = require("./models/url")
 
 const { connectMongooseDb } = require("./connect");
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+const staticRoute = require("./routes/staticRouter")
+
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRoute);
+
+app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
@@ -33,6 +50,8 @@ app.get("/:shortId", async (req, res) => {
 })
 
 app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
 
 connectMongooseDb('mongodb://127.0.0.1:27017/short-url')
     .then(() => console.log("MongoDB Connected"))
