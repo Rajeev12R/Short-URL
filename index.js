e93c5cc20908749782bce67484b48b59d7a4f00a
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-10 14:33:56 +0530

    feat: Setup Express app with routes for URL shortening and MongoDB connection

--- index.js
+++ index.js
@@ -38,7 +38,3 @@ connectMongooseDb('mongodb://127.0.0.1:27017/short-url')
     .then(() => console.log("MongoDB Connected"))
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
-
-
-
-
