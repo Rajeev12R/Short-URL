f05eae418e04aa10c21f568051330932aa08b5bb
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-10 21:52:50 +0530

    Add route handlers for URL operations and User Interface
    - Implement route to fetch and render all URLs
    - Add route for generating new short URLs
    - Add route for retrieving analytics of a specific short URL

---
+++ routes/staticRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const URL  = require("../models/url")
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const allUrls = await URL.find({
+    })
+    return res.render("home", {
+        urls: allUrls,
+
+    });
+})
+
+module.exports = router;
