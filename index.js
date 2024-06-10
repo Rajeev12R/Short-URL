936f583e618e1f79cba3837a812365da76c7a652
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-10 14:16:34 +0530

    main file

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const express = require("express")
+const express = require("express");
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = 8001;
 
-app.listen(PORT, ()=> console.log(`Server Started at PORT: ${PORT}`))
+const urlRoute = require("./routes/url");
+
+const URL = require("./models/url")
+
+const { connectMongooseDb } = require("./connect");
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now()
+            }
+        }
+    });
+    res.redirect(entry.redirectURL)
+})
+
+app.use(express.json());
+
+connectMongooseDb('mongodb://127.0.0.1:27017/short-url')
+    .then(() => console.log("MongoDB Connected"))
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
 
 
 
