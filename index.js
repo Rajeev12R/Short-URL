010445a615ba2e6b0bda530941db61054c9c3cb7
Rajeev Ranjan <135375413+Rajeev12R@users.noreply.github.com>
2024-06-10 23:14:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,5 @@ connectMongooseDb('mongodb://127.0.0.1:27017/short-url')
     .then(() => console.log("MongoDB Connected"))
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`))
+
+module.exports = app;
