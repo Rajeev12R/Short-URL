cb12798ba29dd8886abe10f2cdebc993d933bdb6
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-14 23:16:46 +0530

    Add functions for generating short URL and retrieving analytics

--- controllers/url.js
+++ controllers/url.js
@@ -10,6 +10,7 @@ async function handleGenerateNewShortURL(req, res) {
         shortId: shortID,
         redirectURL: body.url,
         visitHistory: [],
+        createdBy: req.user._id,
 
     });
 
