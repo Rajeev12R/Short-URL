cc33440cfccd7c0c195cbb7a71e007629d4d7f4f
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-14 23:12:08 +0530

    Add routes for serving home, signup, and login pages

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -4,12 +4,19 @@ const URL  = require("../models/url")
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const allUrls = await URL.find({
-    })
+    if(!req.user) return res.redirect("/login");
+    const allUrls = await URL.find({ createdBy: req.user._id})
     return res.render("home", {
         urls: allUrls,
 
     });
 })
+
+router.get("/signup", (req, res) =>{
+    return res.render("signup");
+});
+router.get("/login", (req, res) =>{
+    return res.render("login");
+});
 
 module.exports = router;
