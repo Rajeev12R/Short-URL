3b74e945f9e45f3cbc4500e614fffbf30083f824
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-30 04:07:54 +0530

    Tailwind config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [
+    "./**/*.{html,js,jsx,ts,tsx,vue,svelte,php,blade.php,mdx,ejs}", // Scans all directories and includes ejs files
+  ],
   theme: {
     extend: {},
   },
