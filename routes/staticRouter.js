354d27294a6257217c9fbe18d3e7530dfc40f1a6
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-06-30 04:02:25 +0530

    feat: Intro Page

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,21 +1,24 @@
 const express = require("express");
-
-const URL  = require("../models/url")
+const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    if(!req.user) return res.redirect("/intro");
-    const allUrls = await URL.find({ createdBy: req.user._id})
+    if (!req.user) return res.redirect("/intro");
+    const allUrls = await URL.find({ createdBy: req.user._id });
     return res.render("home", {
         urls: allUrls,
-
     });
-})
+});
 
-router.get("/signup", (req, res) =>{
+router.get("/intro", (req, res) => {
+    return res.render("intro");
+});
+
+router.get("/signup", (req, res) => {
     return res.render("signup");
 });
-router.get("/login", (req, res) =>{
+
+router.get("/login", (req, res) => {
     return res.render("login");
 });
 
